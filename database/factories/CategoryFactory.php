d09ddc41174464eed25dea167c4ca43e078a2c80
Francisco <fcomorales.sanchez@gmail.com>
2021-12-13 22:18:54 -0300

    Datos de prueba Categorias

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            /* hay que indicarle la ruta donde queremos que se almacene la imagen
+            * y las dimensiones como parametro extra
+            */
+            'image' => 'products/'.$this->faker->image('public/storage/products', 640, 480, null, false)
+        ];
+    }
+}
