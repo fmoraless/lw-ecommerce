8920d40b3a7462ad7bbf80d2c6bff0f77e6ee6cb
Francisco <fcomorales.sanchez@gmail.com>
2021-12-20 09:59:34 -0300

    SubCategory seeder

--- database/seeders/SubcategorySeeder.php
+++ database/seeders/SubcategorySeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Subcategory;
 
 class SubcategorySeeder extends Seeder
 {
@@ -13,6 +15,113 @@ class SubcategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $subcategories = [
+            /* Celulares y tablets */
+            [
+                'category_id' => 1,
+                'name' => 'Celulares y smartphones',
+                'slug' => Str::slug('Celulares y smartphones'),
+                'color' => true
+            ],
+            [
+                'category_id' => 1,
+                'name' => 'Accesorios para Celulares',
+                'slug' => Str::slug('Accesorios para Celulares'),
+                
+            ],
+            [
+                'category_id' => 1,
+                'name' => 'Smartwatches',
+                'slug' => Str::slug('Smartwatches'),
+                
+            ],
+            /* TV audio y video */
+            [
+                'category_id' => 2,
+                'name' => 'TV y audio',
+                'slug' => Str::slug('TV y audio'),
+               
+            ],
+            [
+                'category_id' => 2,
+                'name' => 'Audio',
+                'slug' => Str::slug('Audio'),
+                
+            ],
+            [
+                'category_id' => 2,
+                'name' => 'Audio para auto',
+                'slug' => Str::slug('Audio para auto'),
+                
+            ],
+            /* Consola y videojuegos */
+            [
+                'category_id' => 3,
+                'name' => 'Xbox',
+                'slug' => Str::slug('Xbox'),
+            ],
+            [
+                'category_id' => 3,
+                'name' => 'Play station',
+                'slug' => Str::slug('play-station'),
+            ],
+            [
+                'category_id' => 3,
+                'name' => 'Video juegos para PC',
+                'slug' => Str::slug('video-juegos-para-pc'),
+            ],
+            [
+                'category_id' => 3,
+                'name' => 'Nintendo',
+                'slug' => Str::slug('nintendo'),
+            ],
+            /* Computacion */
+            [
+                'category_id' => 4,
+                'name' => 'Portatles',
+                'slug' => Str::slug('portatles'),
+            ],
+            [
+                'category_id' => 4,
+                'name' => 'PC escritorio',
+                'slug' => Str::slug('pc-escritorio'),
+            ],
+            [
+                'category_id' => 4,
+                'name' => 'Almacenamiento',
+                'slug' => Str::slug('almacenamiento'),
+            ],
+            [
+                'category_id' => 4,
+                'name' => 'Accesorios computadoras',
+                'slug' => Str::slug('accesorios-computadoras'),
+            ],
+            /* Moda */
+            [
+                'category_id' => 5,
+                'name' => 'Mujeres',
+                'slug' => Str::slug('mujeres'),
+            ],
+            [
+                'category_id' => 5,
+                'name' => 'Hombres',
+                'slug' => Str::slug('hombres'),
+            ],
+            [
+                'category_id' => 5,
+                'name' => 'Lentes',
+                'slug' => Str::slug('Lentes'),
+            ],
+            [
+                'category_id' => 5,
+                'name' => 'Relojes',
+                'slug' => Str::slug('Relojes'),
+            ],
+
+        ];
+
+        foreach ($subcategories as $subcategory) {
+            Subcategory::factory(1)->create($subcategory);
+        }
     }
 }
