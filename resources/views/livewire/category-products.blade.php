c63eef2fe367654dc58f6789ed1fcba2f630ce98
fmoraless <franticzero@gmail.com>
2024-02-22 22:39:30 -0300

    add glider carousel

---
+++ resources/views/livewire/category-products.blade.php
@@ -0,0 +1,38 @@
+<div wire:init="loadPosts">
+    @if (count($products))
+        <div class="glider-contain">
+            <ul class="glider-{{$category->id}}">
+                @foreach($products as $product)
+                    <li class="bg-white rounded-lg shadow {{ $loop->last ? '' : 'mr-4'}}">
+                        <article>
+                        <figure>
+                            @if($product->images->isNotEmpty())
+                                <img src="{{ Storage::url($product->images->first()->url) }}" alt="">
+                            @else
+                                <img src="{{ asset('images/default-image2.png') }}" alt="Default Image">
+                            @endif
+                        </figure>
+                            <div class="py-4 px-6">
+                                <h1 class="text-lg font-semibold">{{$product->name}}
+                                <a href="">
+                                    {{ Str::limit($product->name, 20)}}
+                                </a>
+                                </h1>
+                                <p class="font-bold text-trueGray-700">${{$product->price}}</p>
+                            </div>
+                        </article>
+                    </li>
+                @endforeach
+            </ul>
+
+            <button aria-label="Previous" class="glider-prev">«</button>
+            <button aria-label="Next" class="glider-next">»</button>
+            <div role="tablist" class="dots"></div>
+        </div>
+    @else
+        <div class="mb-4 h-48 flex justify-center items-center bg-white shadow-xl border border-gray-100 rounded-lg">
+            <div class="rounded animate-spin ease duration-300 w-10 h-10 border-2 border-indigo-500"></div>
+        </div>
+    @endif
+
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,31 @@
 <x-app-layout>
+    <div class="container py-8">
+        @foreach ($categories as $category)
+            <section class="mb-6">
+                <h1 class="text-lg uppercase font-semibold text-gray-700">
+                    {{$category->name}}
+                </h1>
+                @livewire('category-products', ['category' => $category])
+            </section>
+        @endforeach
+
+    </div>
+    @push('script')
+    <script>
+        Livewire.on('glider', function (id) {
+             new Glider(document.querySelector('.glider-' + id), {
+                slidesToShow: 5.5,
+                slidesToScroll: 5,
+                draggable: true,
+                dots: '.dots',
+                arrows: {
+                    prev: '.glider-prev',
+                    next: '.glider-next'
+                }
+            });
+        })
+
+    </script>
+    @endpush
 
 </x-app-layout>
