0360dcab78d38e957185ff8705aff1efbf5e8fff
fmoraless <franticzero@gmail.com>
2024-03-04 14:02:54 -0300

    cart items with color

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -30,7 +30,7 @@ class Product extends Model
 
     /* Relacion muchos a muchos*/
     public function colors(){
-        return $this->belongsToMany(Color::class);
+        return $this->belongsToMany(Color::class)->withPivot('quantity');
     }
 
     /* Relacion uno a muchos*/
