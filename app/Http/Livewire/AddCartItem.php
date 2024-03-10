c75d889849e33d2ae140f5c8346ba8b72d8cfd42
fmoraless <franticzero@gmail.com>
2024-03-10 14:39:19 -0300

    show item quantity on cart

--- app/Http/Livewire/AddCartItem.php
+++ app/Http/Livewire/AddCartItem.php
@@ -37,6 +37,8 @@ class AddCartItem extends Component
             'weight' => 550,
             'options' => $this->options
         ]);
+
+        $this->emitTo('dropdown-cart', 'render');
     }
     public function render()
     {
