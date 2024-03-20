ce077c11bb5f6d3f5f81e09dd344c8e561a89b0c
fmoraless <franticzero@gmail.com>
2024-03-20 19:39:48 -0300

    design create order

--- app/Http/Livewire/CreateOrder.php
+++ app/Http/Livewire/CreateOrder.php
@@ -3,9 +3,20 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Department;
 
 class CreateOrder extends Component
 {
+    public $envio_type = 1;
+    public $address, $reference;
+    public $department, $cities = [], $districts = [];
+    public $department_id = "", $city_id = "", $district_id ="";
+
+
+    public function mount(){
+        $this->departments = Department::all();
+    }
+
     public function render()
     {
         return view('livewire.create-order');
--- resources/views/livewire/create-order.blade.php
+++ resources/views/livewire/create-order.blade.php
@@ -1,3 +1,155 @@
-<div>
-    {{-- The whole world belongs to you. --}}
+<div class="container py-8 grid grid-cols-5 gap-6">
+    <div class="col-span-3">
+
+        <div class="bg-white rounded-lg shadow p-6">
+            <div class="mb-4">
+                <x-jet-label value="Nombre del contacto" />
+                <x-jet-input type="text"
+                    placeholder="Nombre de la persona que recibe el producto"
+                    class="w-full"
+                />
+            </div>
+            <div>
+                <x-jet-label value="Teléfono de contacto" />
+                <x-jet-input type="text"
+                    placeholder="+56-66-6666-66"
+                    class="w-full"
+                />
+            </div>
+        </div>
+
+        <div x-data="{ envio_type: @entangle('envio_type') }">
+            <p class="mt-6 mb-3 text-lg text-gray-700 font-semibold">Envíos</p>
+
+            <label class="bg-white rounded-lg shadow px-6 py-4 flex items-center mb-4">
+                <input x-model="envio_type" type="radio" value="1" name="envio_type" class="text-gray-600">
+                <span class="ml-2 text-gray-700">
+                    Retiro en tieda (Calle lera 234)
+                </span>
+                <span class="font-semibold text-gray-700 ml-auto">
+                    Gratis
+                </span>
+            </label>
+            <div class="bg-white rounded-lg shadow">
+                <label class="px-6 py-4 flex items-center cursor-pointer">
+                    <input x-model="envio_type" type="radio" value="2" name="envio_type" class="text-gray-600">
+                    <span class="ml-2 text-gray-700">
+                        Envío a domicilio
+                    </span>
+                </label>
+
+                <div class="px-6 pb-6 grid grid-cols-2 gap-6 hidden" :class="{'hidden': envio_type != 2}">
+                    {{-- Departamentos --}}
+                    <div>
+                        <x-jet-label value="Departamento" />
+
+                        <select class="form-control w-full" wire:model="department_id">
+                            <option value="" disabled selected>Seleccione un departamento</option>
+                            @foreach ($departments as $department)
+                                <option value="{{$department->id}}">{{$department->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    {{-- Ciudades --}}
+                    <div>
+                        <x-jet-label value="Ciudad" />
+
+                        <select class="form-control w-full" wire:model="city_id">
+                            <option value="" disabled selected>Seleccione ciudad</option>
+                            @foreach ($cities as $city)
+                                <option value="{{$city->id}}">{{$city->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    {{-- Distritos --}}
+                    <div>
+                        <x-jet-label value="Distrito" />
+
+                        <select class="form-control w-full" wire:model="district_id">
+                            <option value="" disabled selected>Seleccione distrito</option>
+                            @foreach ($districts as $district)
+                                <option value="{{$district->id}}">{{$district->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div>
+                        <x-jet-label value="Dirección" />
+                        <x-jet-input wire:model="address" class="w-full" type="text" />
+                    </div>
+                    <div class="col-span-2">
+                        <x-jet-label value="Referencia" />
+                        <x-jet-input wire:model="reference" class="w-full" type="text" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <x-jet-button class="mt-6 mb-4">
+                Continuar con la compra
+            </x-jet-button>
+
+            <hr>
+
+            <p class="text-sm text-gray-700 mt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, rerum incidunt. Quis dolore obcaecati aperiam harum quisquam nihil
+                 vel suscipit iste, velit, perspiciatis in rerum commodi accusamus assumenda ullam aut.
+                 <a href="" class="font-semibold text-orange-500"></a>
+            </p>
+        </div>
+    </div>
+    <div class="col-span-2">
+        <div class="bg-white rounded-lg shadow p-6">
+            <ul>
+                @forelse(Cart::content() as $item)
+                    <li class="flex p-2 border-b border-gray-200">
+                        <img class="h-15 w-20 object-cover mr-4" src="{{$item->options->image}}" alt="">
+                        <article class="flex-1">
+                            <h1 class="font-bold">{{$item->name}}</h1>
+                            <div class="flex">
+                                <p>Cant: {{$item->qty}}</p>
+                                @isset($item->options['color'])
+                                    <p class="mx-2">- Color: {{__($item->options['color'])}}</p>
+                                @endisset
+
+                                @isset($item->options['size'])
+                                    <p>- Talla: {{$item->options['size']}}</p>
+                                @endisset
+                            </div>
+                            <p>USD {{$item->price}}</p>
+                        </article>
+                    </li>
+                @empty
+                    <div class="py-6 px-4">
+                        <p class="text-center text-gray-700">
+                            No tienes productos en el carrito
+                        </p>
+                    </div>
+                @endforelse
+            </ul>
+
+            <hr class="mt-4 mb-3">
+
+            <div class="text-gray-700">
+                <p class="flex justify-between items-center">
+                    Subtotal
+                    <span class="font-semibold">{{Cart::subtotal()}} USD</span>
+                </p>
+                <p class="flex justify-between items-center">
+                    Envío
+                    <span class="font-semibold">Gratis</span>
+                </p>
+
+                <hr class="mt-4 mb-3">
+
+                <p class="flex justify-between items-center font-semibold">
+                    <span class="text-lg">Total</span>
+                    {{Cart::subtotal()}} USD
+                </p>
+
+            </div>
+        </div>
+        </div>
 </div>
